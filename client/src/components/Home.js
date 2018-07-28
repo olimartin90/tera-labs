f3c498688e591aaf32b7c0c565365a9c38340422
Thierry Gribeauval <t.gribeauval11@gmail.com>
2018-07-28 21:39:44 +0000

    Added image folder

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,7 +3,18 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <p>Home</p>
+          <head>
+            <title>TeraLabs</title>
+          </head>
+          <body>
+           <header>
+           <h1> TeraLabs </h1>
+           <p> Spicy jalapeno bacon ipsum dolor amet frankfurter kielbasa filet mignon turducken landjaeger, pancetta chuck shoulder pork belly bacon Ground round drumstick spare ribs filet mignon chuck tongue fatback prosciutto strip steak. Pork filet mignon leberkas swine turkey flank corned beef shank bacon prosciutto ground round t-bone jowl kevin. Corned beef shoulder jowl alcatra short loin pancetta buffalo picanha strip steak pork belly turkey kielbasa chicken salami. Meatball hamburger corned beef short loin, t-bone tri-tip short ribs Venison beef ribs chicken, tongue jowl landjaeger prosciutto shoulder cow pancetta doner. </p>
+           </header>
+           <main>
+           <p> This is my content </p>
+           </main>
+          </body>
         </div>
     )
 }
