5a6986359d4a3e267f812227087764bc77f92fa1
thierrygribeauval <t.gribeauval11@gmail.com>
2018-07-31 16:58:39 -0400

    Finalizing my landing page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -21,84 +21,134 @@ const Home = () => {
          </Row>
 
          <Row class="container1">
-            <Col xs={6} md={4}>
-              <div class="collect">
-                <div class="collecticon">
-                  <i className="fas fa-heart"/>
-                  <h4> Collect </h4>
+            <Col xs={12} md={4}>
+              <div className="sectiondisplay">
+                <div className="iconh4">
+                 <div class="icon">
+                    <i class="fas fa-sitemap"></i>
+                  </div>
+                  <h4 className="h4"> Collect </h4>
                 </div>
-                <p class="codismoncontent"> Spicy jalapeno bacon ipsum dolor amet sirloin prosciutto ham hock ham frankfurter Swine andouille pastrami strip steak Andouille sirloin doner flank pastrami tongue ground round short loin venis</p>
+                <p className="pcontent"> Spicy jalapeno bacon ipsum dolor amet sirloin prosciutto ham hock ham frankfurter Swine andouille pastrami strip steak Andouille sirloin doner flank pastrami tongue ground round short loin venis</p>
               </div>
             </Col>
 
 
-            <Col xs={6} md={4}>
-              <div class="display">
-                <div class="displayicon">
-                  <i className="fa fa-spinner" />
-                  <h4> Display </h4>
+            <Col xs={12} md={4}>
+              <div className="sectiondisplay">
+                <div className="iconh4">
+                  <div class="icon">
+                    <i class="fas fa-chart-bar"></i>
+                  </div>
+                  <h4 className="h4"> Display </h4>
                 </div>
-                <p class="codismoncontent"> Spicy jalapeno bacon ipsum dolor amet sirloin prosciutto ham hock ham frankfurter Swine andouille pastrami strip steak Andouille sirloin doner flank pastrami tongue ground round short loin venis</p>
+                <p className="pcontent"> Spicy jalapeno bacon ipsum dolor amet sirloin prosciutto ham hock ham frankfurter Swine andouille pastrami strip steak Andouille sirloin doner flank pastrami tongue ground round short loin venis</p>
               </div>
             </Col>
 
-            <Col xs={6} md={4}>
-              <div class="monitor">
-                <div class="monitoricon">
-                  <i className="fa fa-spinner" />
-                  <h4> Monitor </h4>
+            <Col xs={12} md={4}>
+              <div className="sectiondisplay">
+                <div className="iconh4">
+                  <div class="icon">
+                    <i class="fas fa-exclamation-circle"></i>
+                  </div>
+                  <h4 className="h4"> Monitor </h4>
                 </div>
-                <p class="codismoncontent"> Spicy jalapeno bacon ipsum dolor amet sirloin prosciutto ham hock ham frankfurter Swine andouille pastrami strip steak Andouille sirloin doner flank pastrami tongue ground round short loin venis </p>
+                <p className="pcontent"> Spicy jalapeno bacon ipsum dolor amet sirloin prosciutto ham hock ham frankfurter Swine andouille pastrami strip steak Andouille sirloin doner flank pastrami tongue ground round short loin venis </p>
               </div>
             </Col>
          </Row>
 
-         <Row>
-          <img src={process.env.PUBLIC_URL + '/graphexample2.jpg'} alt= "" />
-          <div className="sectiontwo">
-            <Col xsHidden md={4}>
-              <div>
-                <h4> Create your account </h4>
-                <i className="fas fa-chart-line" />
-                <p> Spicy jalapeno bacon ipsum dolor amet sirloin prosciutto ham hock ham frankfurter Swine andouille pastrami strip steak Andouille sirloin doner flank pastrami tongue ground round short loin venis</p>
-              </div>
-            </Col>
-            <Col xsHidden md={4}>
-              <div>
-                <h4> Connect your sensors </h4>
-                <i className="fa fa-spinner" />
-                <p>Spicy jalapeno bacon ipsum dolor amet sirloin prosciutto ham hock ham frankfurter Swine andouille pastrami strip steak Andouille sirloin doner flank pastrami tongue ground round short loin venis </p>
-              </div>
-            </Col>
-            <Col xsHidden md={4}>
-              <div>
-                <h4> Visualize your data </h4>
-                <p> Spicy jalapeno bacon ipsum dolor amet sirloin prosciutto ham hock ham frankfurter Swine andouille pastrami strip steak Andouille sirloin doner flank pastrami tongue ground round short loin venis </p>
-              </div>
-            </Col>
-          </div>
-         </Row>
+         <div className="backgroundimage2">
+         </div>
 
          <Row>
-          <h2 className="advancedfeatures"> Advanced Features </h2>
-          <div>
-            <Col xs={6} md={4}>
-              <h4> Locate </h4>
-              <p> Spicy jalapeno bacon ipsum dolor amet sirloin prosciutto ham hock ham frankfurter Swine andouille pastrami strip steak Andouille sirloin doner flank pastrami tongue ground round short loin venis </p>
+         <Col xs={8} sm={8} md={8}>
+          <img className="graphimage img-responsive"src={process.env.PUBLIC_URL + '/graphexample2.jpg'} alt= "" />
+         </Col>
+
+            <Col xs={12} sm={12} md={4}>
+
+
+              <div className="sectiondisplay">
+                <div className="iconh4">
+                  <div class="icon">
+                    <i class="far fa-plus-square"></i>
+                  </div>
+                  <h4 className="h4"> Create your account </h4>
+                </div>
+                <p className="pcontent"> Spicy jalapeno bacon ipsum dolor amet sirloin prosciutto ham hock ham frankfurter Swine andouille pastrami strip steak Andouille sirloin doner flank pastrami tongue ground round short loin venis</p>
+              </div>
+
+
+
+              <div className="sectiondisplay">
+                <div className="iconh4">
+                  <div class="icon">
+                    <i class="fas fa-cogs"></i>
+                  </div>
+                  <h4 className="h4"> Connect your sensors </h4>
+                </div>
+                <p className="pcontent">Spicy jalapeno bacon ipsum dolor amet sirloin prosciutto ham hock ham frankfurter Swine andouille pastrami strip steak Andouille sirloin doner flank pastrami tongue ground round short loin venis </p>
+              </div>
+
+
+
+              <div className="sectiondisplay">
+                <div className="iconh4">
+                  <div class="icon">
+                    <i class="fas fa-eye"></i>
+                  </div>
+                  <h4 className="h4"> Visualize your data </h4>
+                </div>
+                <p className="pcontent"> Spicy jalapeno bacon ipsum dolor amet sirloin prosciutto ham hock ham frankfurter Swine andouille pastrami strip steak Andouille sirloin doner flank pastrami tongue ground round short loin venis </p>
+              </div>
+
+
             </Col>
-          </div>
-          <div>
-            <Col xs={6} md={4}>
-              <h4> Analyse </h4>
-              <p> Spicy jalapeno bacon ipsum dolor amet sirloin prosciutto ham hock ham frankfurter Swine andouille pastrami strip steak Andouille sirloin doner flank pastrami tongue ground round short loin venis </p>
+         </Row>
+
+         <div class="backgroundimage3">
+         </div>
+
+         <Row>
+          <h2 className="advancedfeatures">  Advanced Features </h2>
+            <Col xs={12} md={4}>
+              <div className="sectiondisplay">
+                <div className="iconh4">
+                  <div class="icon">
+                    <i class="fas fa-map-marker-alt"></i>
+                  </div>
+                  <h4 className="h4"> Locate </h4>
+                </div>
+                <p className="pcontent"> Spicy jalapeno bacon ipsum dolor amet sirloin prosciutto ham hock ham frankfurter Swine andouille pastrami strip steak Andouille sirloin doner flank pastrami tongue ground round short loin venis </p>
+              </div>
             </Col>
-          </div>
-          <div>
-            <Col xs={6} md={4}>
-              <h4> Compare </h4>
-              <p> Spicy jalapeno bacon ipsum dolor amet sirloin prosciutto ham hock ham frankfurter Swine andouille pastrami strip steak Andouille sirloin doner flank pastrami tongue ground round short loin venis </p>
+
+
+            <Col xs={12} md={4}>
+              <div className="sectiondisplay">
+                <div className="iconh4">
+                  <div class="icon">
+                    <i class="fas fa-search"></i>
+                  </div>
+                  <h4 className="h4"> Analyse </h4>
+                </div>
+                <p className="pcontent"> Spicy jalapeno bacon ipsum dolor amet sirloin prosciutto ham hock ham frankfurter Swine andouille pastrami strip steak Andouille sirloin doner flank pastrami tongue ground round short loin venis </p>
+              </div>
             </Col>
-          </div>
+
+            <Col xs={12} md={4}>
+              <div className="sectiondisplay">
+                <div className="iconh4">
+                  <div class="icon">
+                    <i class="fas fa-balance-scale"></i>
+                  </div>
+                  <h4 className="h4"> Compare </h4>
+                </div>
+                <p className="pcontent"> Spicy jalapeno bacon ipsum dolor amet sirloin prosciutto ham hock ham frankfurter Swine andouille pastrami strip steak Andouille sirloin doner flank pastrami tongue ground round short loin venis </p>
+              </div>
+            </Col>
          </Row>
         </Grid>
       </div>
