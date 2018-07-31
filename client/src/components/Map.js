d74de20b0732df4fc87f3a3a7d06792d9bb0a82c
Philippe Bolduc <pbolduc2354@gmail.com>
2018-07-31 17:17:59 -0400

    layout and integration of google map done

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,13 +1,40 @@
 import React, {Component} from 'react';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
-class Map extends Component {
+const style = {
+  width: '100%',
+  height: '100%',
+  position: "absolute",
+  border: "1px solid red",
+  zIndex: "3",
+
+}
+
+class SensorMap extends Component {
+
     render() {
         return (
-          <div>
-            <p>Map</p>
-          </div>
+          <div className="embed-responsive map-wrapper">
+            <Map className="embed-responsive-item"
+            google={this.props.google}
+            style={style}
+            initialCenter={{
+              lat: 39.854885,
+              lng: 88.081807
+            }}
+            zoom={15}
+            onClick={this.onMapClicked}
+          >
+      
+              <Marker onClick={this.onMarkerClick}
+                      name={'Current location'} />
+            </Map>
+            </div>
         )
     }
 }
 
-export default Map;
+
+export default GoogleApiWrapper({
+  apiKey: ("AIzaSyCRmv6SaTr9BTMU7yeXHarnU3v5zYGaLMk")
+})(SensorMap)
