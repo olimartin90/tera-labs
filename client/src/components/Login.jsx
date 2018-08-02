ceab55b00b5dc818efdced0b164af0bdeab20df1
Olivier Martin <olimartin90@gmail.com>
2018-08-02 15:37:52 -0400

    fixed and updated the login component with the header and app

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { Component } from 'react-bootstrap';
-import { Modal } from 'react-bootstrap';
-import Register from "./Register";
+import { Redirect } from 'react-router-dom';
+// import { Component } from 'react-bootstrap';
+import { Col, Form, FormGroup, FormControl, ControlLabel, Checkbox, Button } from 'react-bootstrap';
+// import { Modal } from 'react-bootstrap';
+// import Register from "./Register";
 
 const axios = require('axios');
 
@@ -10,18 +12,26 @@ class Login extends Component {
   constructor(props) {
     super(props);
     this.handleLogin = this.handleLogin.bind(this);
+    this.state = {
+      toDashboard: false
+    };
   }
 
   handleLogin(e) {
     e.preventDefault();
-    axios.post('/api/v1/authentication', { // Verify if the /users is the right url with the back end
+    const email = e.target.elements["email"].value
+    const password = e.target.elements["password"].value
+    axios.post('/api/v1/authentication', {
       user: {
-        email: document.getElementById("email").value,
-        password: document.getElementById("password").value,
+        email: email,
+        password: password
       }
     })
       .then(response => {
-        this.props.changePage("delete");
+        // this.props.changePage("delete");
+        this.setState({
+          toDashboard: true
+        })
         this.props.updateCurrentUser(response.data.user.email);
         localStorage.setItem("auth_token", response.data.auth_token);
       })
@@ -30,18 +40,60 @@ class Login extends Component {
       })
   }
 
-  render() {
-    return (
-      <div>
-        <p>Login</p>
-        <form>
-          <input id="email" placeholder="email" />
-          <input id="password" placeholder="password" />
-          <button onClick={this.handleLogin}>Submit</button>
-        </form>
-        <button onClick={() => this.props.changePage("dashboard")}>Go to Dashboard</button>
+  // displayRegister(e) => {
 
+  // }
+
+  render() {
+    if (this.state.toDashboard === true) {
+      return <Redirect to='/dashboard' />
+      // return <h1>Helloo</h1>
+    }
+    return (
+      // <div>
+
+      //   <p>Login</p>
+      //   <form>
+      //     <input id="email" placeholder="email" />
+      //     <input id="password" placeholder="password" />
+      //     <button onClick={this.handleLogin}>Submit</button>
+      //   </form>
+
+      <div className="loginregisterform">
+        <Form onSubmit={this.handleLogin} horizontal>
+          <FormGroup controlId="formHorizontalEmail">
+            <Col componentClass={ControlLabel} sm={2}>
+              Email
+                  </Col>
+            <Col sm={10}>
+              <FormControl name="email" type="email" placeholder="Email" />
+            </Col>
+          </FormGroup>
+
+          <FormGroup controlId="formHorizontalPassword">
+            <Col componentClass={ControlLabel} sm={2}>
+              Password
+                  </Col>
+            <Col sm={10}>
+              <FormControl name="password" type="password" placeholder="Password" />
+            </Col>
+          </FormGroup>
+
+          <FormGroup>
+            <Col smOffset={2} sm={10}>
+              <Checkbox>Remember me</Checkbox>
+            </Col>
+          </FormGroup>
+
+          <FormGroup>
+            <Col smOffset={2} sm={10}>
+              <Button type="submit">Sign in</Button>
+            </Col>
+          </FormGroup>
+          {/* <a hef="#" onClick={}>Register Here /> */}
+        </Form>;
       </div>
+      // </div>
     )
   };
 };
--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import React, { Component } from 'react';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 import { Grid, Row } from 'react-bootstrap';
 import Popup from "reactjs-popup";
 
@@ -24,51 +24,51 @@ class SensorMap extends Component {
     this.state = {
       markers: [
         {
-        name: "sensor1",
-        latitude: 38.844885,
-        longitude: -87.181807
-      },
-      {
-        name: "sensor2",
-        latitude: 40.64885,
-        longitude: -89.191807
-      },
-      {
-        name: "sensor3",
-        latitude: 41.64885,
-        longitude: -89.191807
-      }
-    ],
-    nameValue: "",
-    latitudeValue: 0,
-    longitudeValue: 0
+          name: "sensor1",
+          latitude: 38.844885,
+          longitude: -87.181807
+        },
+        {
+          name: "sensor2",
+          latitude: 40.64885,
+          longitude: -89.191807
+        },
+        {
+          name: "sensor3",
+          latitude: 41.64885,
+          longitude: -89.191807
+        }
+      ],
+      nameValue: "",
+      latitudeValue: 0,
+      longitudeValue: 0
     }
   }
 
   onMarkerClick(props, marker, e) {
     // for (var item of this.state.markers){
-      console.log(props)
+    console.log(props)
     // }
   }
 
   handleValueName = e => {
     console.log(e.target.value)
-    this.setState({nameValue: e.target.value});
+    this.setState({ nameValue: e.target.value });
   }
   handleValueLatitude = e => {
     console.log(e.target.value)
-    this.setState({latitudeValue: e.target.value});
+    this.setState({ latitudeValue: e.target.value });
   }
   handleValueLongitude = e => {
     console.log(e.target.value)
-    this.setState({longitudeValue: e.target.value});
+    this.setState({ longitudeValue: e.target.value });
   }
 
   handleNewMarker = e => {
     console.log(this.state.latitudeValue)
-    const newMarker = {name: this.state.nameValue, latitude: this.state.latitudeValue, longitude: this.state.longitudeValue}
+    const newMarker = { name: this.state.nameValue, latitude: this.state.latitudeValue, longitude: this.state.longitudeValue }
     const addMarker = this.state.markers.concat(newMarker)
-    this.setState({markers: addMarker})
+    this.setState({ markers: addMarker })
     this.state.nameValue = "";
     this.state.latitudeValue = 0;
     this.state.longitudeValue = 0;
@@ -76,70 +76,70 @@ class SensorMap extends Component {
   }
 
 
-    render() {
+  render() {
 
-// *************** return the markers from the state and send it to the final return ****************
-      let markers = this.state.markers
+    // *************** return the markers from the state and send it to the final return ****************
+    let markers = this.state.markers
 
-      const listOfMarkers = markers.map((item) => {
-        return (
-          <Marker onClick={this.onMarkerClick} name={item.name} position={{lat: item.latitude, lng: item.longitude}} /> 
-        )
-      })
-      
-// ***************** final return ***************************
-        return (
-          <div>
-            <div className="embed-responsive map-wrapper">
-            <Row>
-              <Map className="embed-responsive-item"
-                google={this.props.google}
-                style={style}
-                initialCenter={{
-                  lat: 39.854885,
-                  lng: -88.081807
-                }}
-                zoom={7}
-                onClick={this.onMapClicked}
+    const listOfMarkers = markers.map((item, index) => {
+      return (
+        <Marker onClick={this.onMarkerClick} key={index} name={item.name} position={{ lat: item.latitude, lng: item.longitude }} />
+      )
+    })
+
+    // ***************** final return ***************************
+    return (
+      <div>
+        <div className="embed-responsive map-wrapper">
+          <Row>
+            <Map className="embed-responsive-item"
+              google={this.props.google}
+              style={style}
+              initialCenter={{
+                lat: 39.854885,
+                lng: -88.081807
+              }}
+              zoom={7}
+              onClick={this.onMapClicked}
             >
-        
-                <Marker onClick={this.onMarkerClick}
-                        name={'Current location'} />
 
-                {listOfMarkers}
-              </Map>
-            </Row>
-            </div>
-          <Row className="top-cont" >
-            <Popup trigger={<button> Add new sensor</button>} position="right center" modal closeOnDocumentClick>
+              <Marker onClick={this.onMarkerClick}
+                name={'Current location'} />
+
+              {listOfMarkers}
+            </Map>
+          </Row>
+        </div>
+        <Row className="top-cont" >
+          <Popup trigger={<button> Add new sensor</button>} position="right center" modal closeOnDocumentClick>
             {close => (
-            <div>
-              <form onSubmit={this.handleNewMarker}>
-                <label>
-                  Name:
+              <div>
+                <form onSubmit={this.handleNewMarker}>
+                  <label>
+                    Name:
                   <input type="text" value={this.state.nameValue} onChange={this.handleValueName} />
-                </label>
-                <label>
-                  Latitude:
+                  </label>
+                  <label>
+                    Latitude:
                   <input type="number" value={this.state.latitudeValue} onChange={this.handleValueLatitude} />
-                </label>
-                <label>
-                  Longitude:
+                  </label>
+                  <label>
+                    Longitude:
                   <input type="number" value={this.state.longitudeValue} onChange={this.handleValueLongitude} />
-                </label>
-                <input type="submit" value="Submit" />
-                <input type="button" value="close" onClick={() => {
-              console.log('modal closed ')
-              close()
-            }}/>
-              </form>
-            </div>
+                  </label>
+                  <input type="submit" value="Submit" />
+                  <input type="button" value="close" onClick={() => {
+                    console.log('modal closed ')
+                    close()
+                  }} />
+                </form>
+              </div>
             )}
-            </Popup>
-            </Row>
-          </div>
-        )
-    }
+          </Popup>
+        </Row>
+      </div>
+    )
+  }
 }
 
 
